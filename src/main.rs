e921e74169c971ce6b5053c98b96f75c3e3daabd
magnus <magnus.vestvik@tietoevry.com>
2024-06-03 15:12:52 +0200

    fix parser works :)

--- src/main.rs
+++ src/main.rs
@@ -11,8 +11,8 @@ lazy_static! {
 #[allow(dead_code)]
 #[derive(Debug, PartialEq)]
 enum Rangeable {
-    CharLiteral(char),
-    NumLiteral(u8),
+    CharLiteral,
+    NumLiteral,
 }
 
 #[allow(dead_code)]
@@ -35,6 +35,44 @@ enum RegexAST {
     ZeroOrMany(Box<RegexAST>),
     OneOrMany(Box<RegexAST>),
     WhiteSpace,
+    AnyDigit,
+    AnyWord,
+}
+impl RegexAST {
+    fn iterate_inner_element(&self) {
+        match self {
+            RegexAST::CharLiteral(c) => println!("CharLiteral: {}", c),
+            RegexAST::NumLiteral(n) => println!("NumLiteral"),
+            RegexAST::Word(inner) => {
+                println!("Word:");
+                inner.iterate_inner_element(); // recursively handle inner `RegexAST`
+            }
+            RegexAST::Any => println!("Any"),
+            RegexAST::Range(start, end) => {
+                println!("Range:");
+                start.iterate_inner_element(); // recursively handle start
+                end.iterate_inner_element(); // recursively handle end
+            }
+            RegexAST::Sequence(inner_seq) => {
+                println!("Sequence:");
+                for inner_ast in inner_seq {
+                    inner_ast.iterate_inner_element(); // recursively handle inner sequence
+                }
+            }
+            RegexAST::NewLine => println!("NewLine"),
+            RegexAST::ZeroOrMany(inner) => {
+                println!("ZeroOrMany:");
+                inner.iterate_inner_element(); // recursively handle inner `RegexAST`
+            }
+            RegexAST::OneOrMany(inner) => {
+                println!("OneOrMany:");
+                inner.iterate_inner_element(); // recursively handle inner `RegexAST`
+            }
+            RegexAST::WhiteSpace => println!("WhiteSpace"),
+            RegexAST::AnyDigit => println!("AnyDigit"),
+            RegexAST::AnyWord => println!("AnyWord"),
+        }
+    }
 }
 //////// AST ////////
 //////// Semantics ////////
@@ -74,49 +112,63 @@ fn all_letters() -> HashSet<u32> {
 
 //////// Parser ////////
 
-fn parse_regex(text_match: &str) -> Result<RegexAST, String> {
+fn parse_regex(text_match: &str) -> Result<Vec<RegexAST>, String> {
     let mut chars = text_match.chars().peekable();
     let mut sequence = Vec::new();
+    let mut prev: Option<char> = None;
     while let Some(&c) = chars.peek() {
         match c {
             '\\' => {
                 chars.next();
                 if let Some(escaped) = chars.next() {
                     match escaped {
-                        'w' => sequence.push(RegexAST::Word(Box::new(RegexAST::CharLiteral('w')))),
+                        'w' => sequence.push(RegexAST::AnyWord),
                         's' => sequence.push(RegexAST::WhiteSpace),
-                        'd' => sequence.push(RegexAST::NumLiteral(escaped as u8)),
-                        _ => sequence.push(RegexAST::CharLiteral(escaped)),
+                        'd' => sequence.push(RegexAST::AnyDigit),
+                        _ => sequence.push(RegexAST::AnyWord),
                     };
                 }
             }
             '.' => {
                 chars.next();
-                sequence.push(RegexAST::Any); // this needs to be recursivly called or something
+                sequence.push(RegexAST::Any);
             }
             '*' => {
                 chars.next();
-                if let Some(last) = sequence.pop() {
-                    sequence.push(RegexAST::ZeroOrMany(Box::new(last)));
+                if let Some(prev_char) = prev {
+                    sequence.pop();
+                    let parsed = parse_regex(&prev_char.to_string())?;
+                    if let Some(ast) = parsed.into_iter().next() {
+                        sequence.push(RegexAST::ZeroOrMany(Box::new(ast)));
+                    } else {
+                        return Err("Error parsing previous character".to_string());
+                    }
                 } else {
                     return Err("Syntax error: '*' found without preceding element.".to_string());
                 }
             }
             '+' => {
                 chars.next();
-                if let Some(last) = sequence.pop() {
-                    sequence.push(RegexAST::OneOrMany(Box::new(last)));
+                if let Some(prev_char) = prev {
+                    sequence.pop();
+                    let parsed = parse_regex(&prev_char.to_string())?;
+                    if let Some(ast) = parsed.into_iter().next() {
+                        sequence.push(RegexAST::OneOrMany(Box::new(ast)));
+                    } else {
+                        return Err("Error parsing previous character".to_string());
+                    }
                 } else {
                     return Err("Syntax error: '+' found without preceding element.".to_string());
                 }
             }
             _ => {
                 chars.next();
-                sequence.push(RegexAST::CharLiteral(c)); // this also needs to be recursivly called or something
+                sequence.push(RegexAST::CharLiteral(c));
             }
         };
+        prev = Some(c);
     }
-    return Ok(RegexAST::Sequence(sequence));
+    Ok(sequence)
 }
 
 //////// Parser ////////
@@ -137,45 +189,41 @@ mod tests {
     #[test]
     fn test_parse_literal() {
         let pattern = "a";
-        let expected = RegexAST::Sequence(vec![RegexAST::CharLiteral('a')]);
+        let expected = vec![RegexAST::CharLiteral('a')];
         assert_eq!(parse_regex(pattern), Ok(expected));
     }
 
     #[test]
     fn test_parse_any() {
         let pattern = ".";
-        let expected = RegexAST::Sequence(vec![RegexAST::Any]);
+        let expected = vec![RegexAST::Any];
         assert_eq!(parse_regex(pattern), Ok(expected));
     }
 
     #[test]
     fn test_parse_zero_or_many() {
         let pattern = "a*";
-        let expected = RegexAST::Sequence(vec![RegexAST::ZeroOrMany(Box::new(
-            RegexAST::CharLiteral('a'),
-        ))]);
+        let expected = vec![RegexAST::ZeroOrMany(Box::new(RegexAST::CharLiteral('a')))];
         assert_eq!(parse_regex(pattern), Ok(expected));
     }
 
     #[test]
     fn test_parse_one_or_many() {
         let pattern = "a+";
-        let expected = RegexAST::Sequence(vec![RegexAST::OneOrMany(Box::new(
-            RegexAST::CharLiteral('a'),
-        ))]);
+        let expected = vec![RegexAST::OneOrMany(Box::new(RegexAST::CharLiteral('a')))];
         assert_eq!(parse_regex(pattern), Ok(expected));
     }
 
     #[test]
     fn test_parse_combined() {
         let pattern = "a.*b+c\\d";
-        let expected = RegexAST::Sequence(vec![
+        let expected = vec![
             RegexAST::CharLiteral('a'),
             RegexAST::ZeroOrMany(Box::new(RegexAST::Any)),
-            RegexAST::CharLiteral('b'),
-            RegexAST::OneOrMany(Box::new(RegexAST::CharLiteral('c'))),
-            RegexAST::Word(Box::new(RegexAST::CharLiteral('d'))),
-        ]);
+            RegexAST::OneOrMany(Box::new(RegexAST::CharLiteral('b'))),
+            RegexAST::CharLiteral('c'),
+            RegexAST::AnyDigit,
+        ];
         assert_eq!(parse_regex(pattern), Ok(expected));
     }
 }
