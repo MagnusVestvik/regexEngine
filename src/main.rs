457763b07d00b3f52a897da1352714fd3520515c
magnus <magnus.vestvik@tietoevry.com>
2024-06-03 12:15:59 +0200

    feat updated parser and add tests

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,6 @@ use lazy_static::lazy_static;
 use std::collections::HashSet;
 use std::str::Chars;
 
-/*
-* bools array med true hvor det er match false ellers sjekker deretter lengden av true for å se om
-* det stemmer overens med lengden av pattern.
-*/
 //////// CONSTANTS ////////
 lazy_static! {
     static ref ALL_CHARS: HashSet<char> = num_sequence_to_char(all_letters());
@@ -13,27 +9,31 @@ lazy_static! {
 ///// CONSTANTS ////////
 //////// AST ////////
 #[allow(dead_code)]
+#[derive(Debug, PartialEq)]
 enum Rangeable {
     CharLiteral(char),
     NumLiteral(u8),
 }
 
 #[allow(dead_code)]
+#[derive(Debug, PartialEq)]
 enum Word {
     WhiteSpace,
     Range(Rangeable),
 }
+
 #[allow(dead_code)]
+#[derive(Debug, PartialEq)]
 enum RegexAST {
     CharLiteral(char),
     NumLiteral(u8),
     Word(Box<RegexAST>),
-    Any(Box<RegexAST>),
+    Any,
     Range(Box<RegexAST>, Box<RegexAST>),
     Sequence(Vec<RegexAST>),
     NewLine,
-    ZeroOrMany,
-    OneOrMany,
+    ZeroOrMany(Box<RegexAST>),
+    OneOrMany(Box<RegexAST>),
     WhiteSpace,
 }
 //////// AST ////////
@@ -80,25 +80,40 @@ fn parse_regex(text_match: &str) -> Result<RegexAST, String> {
     while let Some(&c) = chars.peek() {
         match c {
             '\\' => {
+                chars.next();
                 if let Some(escaped) = chars.next() {
                     match escaped {
-                        'w' => {
-                            sequence.push(RegexAST::Word(Box::new(RegexAST::CharLiteral(escaped))))
-                        }
+                        'w' => sequence.push(RegexAST::Word(Box::new(RegexAST::CharLiteral('w')))),
                         's' => sequence.push(RegexAST::WhiteSpace),
-                        'd' => sequence.push(RegexAST::NumLiteral(c as u8)),
-                        _ => sequence.push(RegexAST::CharLiteral(c)),
+                        'd' => sequence.push(RegexAST::NumLiteral(escaped as u8)),
+                        _ => sequence.push(RegexAST::CharLiteral(escaped)),
                     };
                 }
             }
             '.' => {
-                sequence.push(RegexAST::Box::new(Any)); // this needs to be recursivly called or
-                                                        // somthing
+                chars.next();
+                sequence.push(RegexAST::Any); // this needs to be recursivly called or something
             }
-            '*' => sequence.push(RegexAST::ZeroOrMany),
-            '+' => sequence.push(RegexAST::OneOrMany),
-            _ => sequence.push(Rangeable::CharLiteral(c)), // this also needs to be recursivly
-                                                           // called or somthing
+            '*' => {
+                chars.next();
+                if let Some(last) = sequence.pop() {
+                    sequence.push(RegexAST::ZeroOrMany(Box::new(last)));
+                } else {
+                    return Err("Syntax error: '*' found without preceding element.".to_string());
+                }
+            }
+            '+' => {
+                chars.next();
+                if let Some(last) = sequence.pop() {
+                    sequence.push(RegexAST::OneOrMany(Box::new(last)));
+                } else {
+                    return Err("Syntax error: '+' found without preceding element.".to_string());
+                }
+            }
+            _ => {
+                chars.next();
+                sequence.push(RegexAST::CharLiteral(c)); // this also needs to be recursivly called or something
+            }
         };
     }
     return Ok(RegexAST::Sequence(sequence));
@@ -114,6 +129,57 @@ fn test_custom_sequence() {
     println!("Number Range: {:?}", num_range);
     println!("Printable Character Range: {:?}", printable_char_range);
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_parse_literal() {
+        let pattern = "a";
+        let expected = RegexAST::Sequence(vec![RegexAST::CharLiteral('a')]);
+        assert_eq!(parse_regex(pattern), Ok(expected));
+    }
+
+    #[test]
+    fn test_parse_any() {
+        let pattern = ".";
+        let expected = RegexAST::Sequence(vec![RegexAST::Any]);
+        assert_eq!(parse_regex(pattern), Ok(expected));
+    }
+
+    #[test]
+    fn test_parse_zero_or_many() {
+        let pattern = "a*";
+        let expected = RegexAST::Sequence(vec![RegexAST::ZeroOrMany(Box::new(
+            RegexAST::CharLiteral('a'),
+        ))]);
+        assert_eq!(parse_regex(pattern), Ok(expected));
+    }
+
+    #[test]
+    fn test_parse_one_or_many() {
+        let pattern = "a+";
+        let expected = RegexAST::Sequence(vec![RegexAST::OneOrMany(Box::new(
+            RegexAST::CharLiteral('a'),
+        ))]);
+        assert_eq!(parse_regex(pattern), Ok(expected));
+    }
+
+    #[test]
+    fn test_parse_combined() {
+        let pattern = "a.*b+c\\d";
+        let expected = RegexAST::Sequence(vec![
+            RegexAST::CharLiteral('a'),
+            RegexAST::ZeroOrMany(Box::new(RegexAST::Any)),
+            RegexAST::CharLiteral('b'),
+            RegexAST::OneOrMany(Box::new(RegexAST::CharLiteral('c'))),
+            RegexAST::Word(Box::new(RegexAST::CharLiteral('d'))),
+        ]);
+        assert_eq!(parse_regex(pattern), Ok(expected));
+    }
+}
+
 //////// Tests ////////
 
 fn main() {
